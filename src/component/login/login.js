fa97386f83693d412517a9734c23d7f1d9c140af
hanhan0914 <amy8611020923@gmail.com>
2023-04-13 23:49:09 +0800

    modify RWD and code review

--- src/component/login/login.js
+++ src/component/login/login.js
@@ -4,13 +4,11 @@ import { faRightToBracket } from '@fortawesome/free-solid-svg-icons';
 import { LoginButton, Background, Title } from './login_style';
 
 function Login() {
+  //點選login後 跳轉到github授權後 會先跳到loading頁拿token存在cookie中 跳轉到REDIRECT_URI(即list頁面)
+  // 用戶登入github後如何(從輸入的email跟password中)拿到client id and client uri -->目前寫死
   // eslint-disable-next-line no-undef
   const CLIENT_ID = process.env.REACT_APP_GITHUB_CLIENT_ID;
   const REDIRECT_URI = 'http://localhost:3000/loading';
-  //點選login後 跳轉到github授權後會先跳到loading頁拿token存在cookie中後跳轉到REDIRECT_URI(也就是list頁面)
-
-  // 用戶登入github後如何(從輸入的email跟password中)拿到client id and client uri -->目前寫死
-  // 拿回後設定scope看要拿取哪些資訊 --> 跳轉回list頁面需要navbar/username/logout
 
   return (
     <>
@@ -27,11 +25,30 @@ function Login() {
             <FontAwesomeIcon icon={faRightToBracket} />
           </a>
         </LoginButton>
-        <img
-          src='images/cat.png'
-          width={'300px'}
-          style={{ marginLeft: '105px', marginTop: '205px' }}
-        />
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            position: 'relative',
+            left: '50%',
+            transform: 'translate(-50%, -32%)',
+            marginTop: '200px',
+          }}
+        >
+          <img
+            src='images/cat.png'
+            width={'300px'}
+            height={'300px'}
+            style={
+              {
+                // position: 'relative',
+                // left: '34%',
+                // transform: 'translate(-50%, -3%)',
+                // marginTop: '200px',
+              }
+            }
+          />
+        </div>
       </Background>
     </>
   );
