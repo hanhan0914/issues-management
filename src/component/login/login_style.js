a2c2dccf3fcf227901820de8768365735dcc42e2
hanhan0914 <amy8611020923@gmail.com>
2023-04-13 23:50:57 +0800

    modify RWD and code review

--- src/component/login/login_style.js
+++ src/component/login/login_style.js
@@ -2,11 +2,26 @@ import styled from 'styled-components';
 
 export const Title = styled.div`
   font-family: serif;
-  font-size: 100px;
+  font-size: 90px;
   font-weight: bolder;
   margin: 50px;
-  margin-left: 100px;
   color: #000000;
+  height: 80px;
+  line-height: 0.8;
+
+  @media screen and (max-width: 825px) {
+    height: 130px;
+    // word-wrap: break-word;
+    font-size: 80px;
+    line-height: 0.8;
+  }
+
+  @media screen and (max-width: 506px) {
+    height: 200px;
+    font-size: 80px;
+    margin: 45px;
+    line-height: 0.8;
+  }
 `;
 
 export const LoginButton = styled.button`
@@ -14,17 +29,17 @@ export const LoginButton = styled.button`
   height: 80px;
   position: relative;
   left: 50%;
-  transform: translate(-50%, -50%);
+  transform: translate(-50%, 470%);
+  cursor: pointer;
+  z-index: 2;
 
   // button 屬性修改
-  font-weight: 900;
+  font-weight: border;
   background-color: transparent;
   font-size: 35px;
   font-family: serif;
   color: white;
   border: none;
-  // background-image: linear-gradient(to right, #003973, #e5e5be);
-
   border-radius: 15px;
 `;
 
@@ -33,16 +48,6 @@ export const Background = styled.div`
   height: 100%;
 `;
 
-export const Word = styled.p`
-  //   // 水平置中
-  position: relative;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 200px;
-  height: 100px;
-`;
-
 //linear-gradient(to right, #283048, #859398)
 // linear-gradient(to right, #2bc0e4, #eaecc6);
-
 // linear-gradient(to right, #085078, #85d8ce);
