8bedda5e8715de6468e8f47f3f075a7f116ab6cc
hanhan0914 <amy8611020923@gmail.com>
2023-05-03 21:57:52 +0800

    Use styled-component to integrate css/RWD

--- src/component/detail/detail_style.js
+++ src/component/detail/detail_style.js
@@ -26,10 +26,26 @@ export const Body = styled.div`
   z-index: 10;
   border: solid 2px #7a7281;
   border-radius: 10px;
+  @media screen and (max-width: 807px) {
+    width: 700px;
+    bottom: 550px;
+  }
 `;
 export const Edit = styled.div`
   margin-left: 580px;
   margin-top: 3px;
+  @media screen and (max-width: 807px) {
+    margin-left: 500px;
+    margin-top: 3px;
+  }
+  @media screen and (max-width: 813px) {
+    margin-left: 450px;
+    margin-top: 3px;
+  }
+  @media screen and (max-width: 632px) {
+    margin-left: 300px;
+    margin-top: 3px;
+  }
 `;
 
 export const Editbutton = styled.button`
@@ -38,10 +54,14 @@ export const Editbutton = styled.button`
   height: 30px;
   border: none;
   border-radius: 5px;
-  font-size: 25px;
+  font-size: 20px;
   font-weight: bold;
   font-family: serif;
   cursor: pointer;
+  @media screen and (max-width: 632px) {
+    width: 60px;
+    height: 30px;
+  }
 `;
 
 export const Title = styled.div`
@@ -103,7 +123,7 @@ export const InputBody = styled.textarea`
 
 export const Popup = styled.div`
   width: 300px;
-  height: 100px;
+  height: 150px;
   border: solid 2px #408080;
   background-color: #d1e9e9;
   border-radius: 5px;
